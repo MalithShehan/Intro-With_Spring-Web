b28f8c0359337d029852c20d434c01d04f45799c
MalithShehan <shehansandakalum2003@gmail.com>
2024-08-18 12:35:26 +0530

    JSONToDTO

--- src/main/java/lk/ijse/gdse68/introwith_springweb/introwithspringweb/config/WebAppConfig.java
+++ src/main/java/lk/ijse/gdse68/introwith_springweb/introwithspringweb/config/WebAppConfig.java
@@ -2,8 +2,10 @@ package lk.ijse.gdse68.introwith_springweb.introwithspringweb.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan(basePackages = "lk.ijse.gdse68.introwith_springweb.introwithspringweb")
+@EnableWebMvc
 public class WebAppConfig {
 }
--- src/main/java/lk/ijse/gdse68/introwith_springweb/introwithspringweb/dto/Customer.java
+++ src/main/java/lk/ijse/gdse68/introwith_springweb/introwithspringweb/dto/Customer.java
@@ -5,15 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class Customer {
-    @JsonProperty("id")
+public class Customer implements Serializable {
     private String id;
-    @JsonProperty("name")
     private String name;
-    @JsonProperty("email")
     private String email;
 
 }
