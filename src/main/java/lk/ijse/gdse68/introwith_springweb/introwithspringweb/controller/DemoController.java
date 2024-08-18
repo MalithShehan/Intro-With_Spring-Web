ea7b36e83a4b9a18f328d4486a7a33cf20bb4c7b
MalithShehan <shehansandakalum2003@gmail.com>
2024-08-18 09:30:59 +0530

    RequestParam

--- src/main/java/lk/ijse/gdse68/introwith_springweb/introwithspringweb/controller/DemoController.java
+++ src/main/java/lk/ijse/gdse68/introwith_springweb/introwithspringweb/controller/DemoController.java
@@ -1,6 +1,8 @@
 package lk.ijse.gdse68.introwith_springweb.introwithspringweb.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/demo")
@@ -9,5 +11,13 @@ public class DemoController {
     @GetMapping(value = "/health")
     public String healthCheck() {
         return "DemoController run perfectly";
+    }
+    @GetMapping(value = "/patternRegex/{id:C\\d{3}}")
+    public String pathvariableWithRegex(@PathVariable("id") String id) {
+        return "DemoController run perfectly with :"+id;
+    }
+    @GetMapping(params = "test=all")
+    public String params() {
+        return "All are tested";
     }
 }
